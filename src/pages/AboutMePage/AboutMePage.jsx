a460832fe8fcb6f3cd8897c34b2fd1df35f02731
pgarcia86 <pgarciabarros86@gmail.com>
2023-06-26 22:25:30 +0200

    Logos

--- src/pages/AboutMePage/AboutMePage.jsx
+++ src/pages/AboutMePage/AboutMePage.jsx
@@ -65,7 +65,7 @@ export default function AboutMePage({ handleIsntHome, isntHome, setIsntHome }) {
       <div id='my__techs'>
         <img
           className='tech__logo'
-          src='https://res.cloudinary.com/dnicdtxe7/image/upload/v1687810815/JavaLogo_xxhwvf.png'
+          src='https://res.cloudinary.com/dnicdtxe7/image/upload/v1687811094/LogoJava2_vvwa0p.png'
           alt='Java Logo'
         />
         <img
